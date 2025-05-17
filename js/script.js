296f722599082882d243ae74d5f90aaac3b008e4
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-05-17 16:18:22 -0400

    s

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 // Created on: Apr 2025
 // This file contains the JS functions for index.html
 
-"use strict"
+"use strict";
 
 /**
  * Check servie worker.
@@ -12,60 +12,63 @@
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICD2O-Unit-6-02-Isaac-Ip/sw.js", {
     scope: "/ICD2O-Unit-6-02-Isaac-Ip/",
-  })
-}
+  });
+
 
 /**
  * This function selects a random number depending on the user's choice.
  */
 function findMovieType() {
-
   // input
-  const age = parseInt(document.getElementById('age').value)
+  const age = parseInt(document.getElementById("age").value);
   // store age in localStorage
-  localStorage.setItem('age', age)
-  let ageStorage = localStorage.getItem('age')
+  localStorage.setItem("age", age);
+  let ageStorage = parseInt(localStorage.getItem("age"));
   // process
   if (ageStorage >= 17) {
     // output
-    document.getElementById('result').innerHTML = 'You can watch an R movie alone.'
-
+    document.getElementById("result").innerHTML =
+      "You can watch an R movie alone.";
   } else if (ageStorage >= 13) {
     // output
-    document.getElementById('result').innerHTML = 'You can watch a PG-13 movie alone.'
-
+    document.getElementById("result").innerHTML =
+      "You can watch a PG-13 movie alone.";
   } else if (ageStorage >= 5) {
     // output
-    document.getElementById('result').innerHTML = 'You can watch a G or PG movie alone.'
-
+    document.getElementById("result").innerHTML =
+      "You can watch a G or PG movie alone.";
   } else {
     // output
-    document.getElementById('result').innerHTML = 'You are too young to watch movies.'
+    document.getElementById("result").innerHTML =
+      "You are too young to watch movies.";
+    // Thank you message
+      document.getElementById("thank-you-message").innerHTML =
+        "Thank you for verifying your age.";
+    }
   }
-  // Thank you message
-  document.getElementById('thank-you-message').innerHTML = 'Thank you for verifying your age.'
-}
 
 function updateAge() {
-  // Update the cookie count
-  let ageStorage = localStorage.getItem('age')
+  let ageStorage = parseInt(localStorage.getItem("age"));
+
   // process
   if (ageStorage >= 17) {
     // output
-    document.getElementById('result').innerHTML = 'You can watch an R movie alone.'
-
+    document.getElementById("result").innerHTML =
+      "You can watch an R movie alone.";
   } else if (ageStorage >= 13) {
     // output
-    document.getElementById('result').innerHTML = 'You can watch a PG-13 movie alone.'
-
+    document.getElementById("result").innerHTML =
+      "You can watch a PG-13 movie alone.";
   } else if (ageStorage >= 5) {
     // output
-    document.getElementById('result').innerHTML = 'You can watch a G or PG movie alone.'
-
+    document.getElementById("result").innerHTML =
+      "You can watch a G or PG movie alone.";
   } else {
     // output
-    document.getElementById('result').innerHTML = 'You are too young to watch movies.'
+    document.getElementById("result").innerHTML =
+      "You are too young to watch movies.";
   }
   // Thank you message
-  document.getElementById('thank-you-message').innerHTML = 'Thank you for verifying your age.'
+  document.getElementById("thank-you-message").innerHTML =
+    "Thank you for verifying your age.";
 }
