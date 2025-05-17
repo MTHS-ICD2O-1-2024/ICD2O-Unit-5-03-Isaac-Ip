305635be57908d1a085cb2d3382029b14afbbb4e
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-05-17 15:59:53 -0400

    load

--- js/script.js
+++ js/script.js
@@ -45,3 +45,27 @@ function findMovieType() {
   // Thank you message
   document.getElementById('thank-you-message').innerHTML = 'Thank you for verifying your age.'
 }
+
+function updateAge() {
+  // Update the cookie count
+  let ageStorage = localStorage.getItem('age')
+  // process
+  if (ageStorage >= 17) {
+    // output
+    document.getElementById('result').innerHTML = 'You can watch an R movie alone.'
+
+  } else if (ageStorage >= 13) {
+    // output
+    document.getElementById('result').innerHTML = 'You can watch a PG-13 movie alone.'
+
+  } else if (ageStorage >= 5) {
+    // output
+    document.getElementById('result').innerHTML = 'You can watch a G or PG movie alone.'
+
+  } else {
+    // output
+    document.getElementById('result').innerHTML = 'You are too young to watch movies.'
+  }
+  // Thank you message
+  document.getElementById('thank-you-message').innerHTML = 'Thank you for verifying your age.'
+}
